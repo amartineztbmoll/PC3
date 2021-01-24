d3384a13415b46fcecd02899ee201f38facc9e00
amartineztbmoll <72513644+amartineztbmoll@users.noreply.github.com>
2021-01-24 18:28:28 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
 
 
-function validar(){
-    var error=true;
-    var name=document.getElementById("name");
-    var surname=document.getElementById("sname");
-    var email=document.getElementById("email");
-    var email2=document.getElementById("email2");
-    var username=document.getElementById("uname");
-    var tlf=document.getElementById("tlf");
+window.onload = function (){
 
-    if(name.lenght>5){
-        error=false;
+    document.getElementById("sign").onclick = function () {
+        validarun();
+        var error=false;
+        var name=document.getElementById("name").value;
+        var sname=document.getElementById("sname").value;
+        var email=document.getElementById("email").value;
+        var email2=document.getElementById("email2").value;
+        var passwd=document.getElementById("passwd").value;
+        var passwd2=document.getElementById("passwd2").value;
+        var uname=document.getElementById("uname").value;
+        var tlf=document.getElementById("tlf").value;
+        
+        
+        if(name.length>20){
+          error=true;
+          document.getElementById("errorn").innerHTML="Longitud màxima superada";
+        }
+        if(sname.length>30){
+            error=true;
+            document.getElementById("errors").innerHTML="Longitud màxima superada";
+          }
+        validarun();
+        
+
+          if(error==true){
+              console.log("Form incorrecte");
+          }else{
+            console.log(name,sname,passwd,email,email2,uname,tlf);
+          }
+      
     }
 
-    if(error==false){
-        alert("correcte");
-    }else{
-        //si no donara error
-        alert("Incorrecte");
-    }
+}
+function validarun(){
+let uname=document.getElementById("uname").value;
+var expreg = /^[u]{1}[0-9]{6}[A-Z]{1}$/;
+if(uname==expreg){
+    alert("correcte");
+}else{
+    alert("incorrecte");
 }
 
 
-    document.getElementById("submit").addEventListener("click", validar);
+}
+
+    
   
